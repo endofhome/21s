3f74ac572be95d6f0a1e5979188d7ed5f80bcfbd
Tom Barnes <ruralmyth@gmail.com>
2016-02-10 00:06:46 +0000

    cards can be shuffled

--- spec/21s.spec.js
+++ spec/21s.spec.js
@@ -8,13 +8,39 @@ describe('Game', function() {
     game.createDeck();
   });
 
-  it('models a deck of 52 cards', function() {
-    expect(game.deck.length).toEqual(52);
+  describe('Initialisation', function() {
+
+    it('models a deck of 52 cards', function() {
+      expect(game.deck.length).toEqual(52);
+    });
+
+    it('there are two players, Sam and the Dealer', function() {
+      expect(game.players[0]).toEqual('Sam');
+      expect(game.players[1]).toEqual('the Dealer');
+    });
+
   });
 
-  it('there are two players, Sam and the Dealer', function() {
-    expect(game.players[0]).toEqual('Sam');
-    expect(game.players[1]).toEqual('the Dealer');
-  });
+  describe("Shuffling the deck", function() {
 
+    beforeEach(function() {
+      spyOn(Math, "random").and.returnValue(0.3);
+    });
+
+    it("4 of Hearts is on top when math.random() returns 0.3", function() {
+      game.shuffleDeck(game.deck);
+      expect(game.deck[0]).toEqual('4 of Hearts');
+    });
+
+    it("3 of Diamonds is on the bottom when math.random() returns 0.3", function() {
+      game.shuffleDeck(game.deck);
+      expect(game.deck[51]).toEqual('3 of Diamonds');
+    });
+
+    it("Ace of Spades is twelfth card in to the stack when math.random() returns 0.3", function() {
+      game.shuffleDeck(game.deck);
+      expect(game.deck[11]).toEqual('Ace of Spades');
+    });
+
+  });
 });
--- src/21s.js
+++ src/21s.js
@@ -8,9 +8,23 @@ Game.prototype.createDeck = function() {
       ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
   for (i = 0; i<4; i++) {
     for (j = 0; j<13; j++) {
-      this.deck.push((ranks[j]) + ' of ' + (suits[i]))
-    };
-  };
+      this.deck.push((ranks[j]) + ' of ' + (suits[i]));
+    }
+  }
+};
+
+Game.prototype.shuffleDeck = function(deckArray) {
+  var currentIndex = deckArray.length,
+      temporaryValue = 0,
+      randomIndex = 0;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = deckArray[currentIndex];
+    deckArray[currentIndex] = deckArray[randomIndex];
+    deckArray[randomIndex] = temporaryValue;
+  }
+  return deckArray;
 };
 
 module.exports = Game;
