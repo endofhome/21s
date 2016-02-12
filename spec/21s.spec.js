5d0fe71b2f24598c1acdb31a9b4ca91a852a7810
Tom Barnes <ruralmyth@gmail.com>
2016-02-12 23:35:29 +0000

    first hand can be dealt

--- spec/21s.spec.js
+++ spec/21s.spec.js
@@ -23,26 +23,35 @@ describe('Game', function() {
 
   });
 
-  describe("Shuffling the deck", function() {
+  describe('Shuffling and dealing the deck', function() {
 
     beforeEach(function() {
-      spyOn(Math, "random").and.returnValue(0.3);
+      spyOn(Math, 'random').and.returnValue(0.3);
     });
 
-    it("4 of Hearts is on top when Math.random() returns 0.3", function() {
+    it('4 of Hearts is on top when Math.random() returns 0.3', function() {
       game.shuffleDeck(game.deck);
       expect(game.deck[0]).toEqual('4 of Hearts');
     });
 
-    it("3 of Diamonds is on the bottom when Math.random() returns 0.3", function() {
+    it('3 of Diamonds is on the bottom when Math.random() returns 0.3', function() {
       game.shuffleDeck(game.deck);
       expect(game.deck[51]).toEqual('3 of Diamonds');
     });
 
-    it("Ace of Spades is twelfth card in to the stack when Math.random() returns 0.3", function() {
+    it('Ace of Spades is twelfth card in to the stack when Math.random() returns 0.3', function() {
       game.shuffleDeck(game.deck);
       expect(game.deck[11]).toEqual('Ace of Spades');
     });
+
+    it('Players are dealt two alternate cards from top of deck', function() {
+      game.shuffleDeck(game.deck);
+      game.dealFirstHand(game.deck);
+      expect(game.players[0].hand[0]).toEqual('4 of Hearts');
+      expect(game.players[0].hand[1]).toEqual('Ace of Clubs');
+      expect(game.players[1].hand[0]).toEqual('7 of Diamonds');
+      expect(game.players[1].hand[1]).toEqual('5 of Spades');      
+    }); 
 
   });
 });
--- src/21s.js
+++ src/21s.js
@@ -10,25 +10,33 @@ Game.prototype.createDeck = function() {
       ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'],
       i = 0,
       j = 0;
-  for (i = 0; i<4; i++) {
-    for (j = 0; j<13; j++) {
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 13; j++) {
       this.deck.push((ranks[j]) + ' of ' + (suits[i]));
     }
   }
 };
 
-Game.prototype.shuffleDeck = function(deckArray) {
-  var currentIndex = deckArray.length,
+Game.prototype.shuffleDeck = function() {
+  var currentIndex = this.deck.length,
       temporaryValue = 0,
       randomIndex = 0;
   while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-    temporaryValue = deckArray[currentIndex];
-    deckArray[currentIndex] = deckArray[randomIndex];
-    deckArray[randomIndex] = temporaryValue;
+    temporaryValue = this.deck[currentIndex];
+    this.deck[currentIndex] = this.deck[randomIndex];
+    this.deck[randomIndex] = temporaryValue;
   }
-  return deckArray;
+  return this.deck;
+};
+
+Game.prototype.dealFirstHand = function() {
+  var i;
+  for (i = 0; i < 4; i += 2) {
+    this.players[0].hand.push(this.deck.shift(i));
+    this.players[1].hand.push(this.deck.shift((i + 1)));
+  };
 };
 
 module.exports = Game;
