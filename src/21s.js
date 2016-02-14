703751cdba86c5d34f21736a55d238204ce50d59
Tom Barnes <ruralmyth@gmail.com>
2016-02-14 00:46:36 +0000

    refractoring

--- src/21s.js
+++ src/21s.js
@@ -61,11 +61,15 @@ Game.prototype.scoreHand = function(player) {
   points.reduce(function(previousValue, currentValue, currentIndex, array) {
     score = previousValue + currentValue;
   });
+  return (checkIfPerfectScore(player, score));
+};
+
+var checkIfPerfectScore = function(player, score) {
   if (score === 21) {
     return player.name + ' has won!';
   } else {
     return score
-  };
+  };  
 };
 
 module.exports = Game;
