396a1a972cd87632156e3f2b0081d9c31d040362
Tom Barnes <ruralmyth@gmail.com>
2016-02-27 11:27:02 +0000

    add test for Player.hand

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -12,5 +12,12 @@ describe('Player', function() {
     it('has a name', function() {
       expect(player.name).toEqual('Sam');
     });
+
+    it('holds a hand of cards', function() {
+      player.hand.push('4 of Clubs');
+      player.hand.push('9 of Hearts');
+      player.hand.push('Ace of Spades');
+      expect(player.hand).toEqual(['4 of Clubs', '9 of Hearts', 'Ace of Spades']);
+    });
   });
 });
